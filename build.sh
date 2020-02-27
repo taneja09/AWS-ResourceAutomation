a13a528e2e3ae15c41aa75a6836bf1cf97ef918b
taneja09 <taneja.di@northeastern.edu>
2020-02-27 15:00:44 -0500

    added capabilities to build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ read -p "please enter stack name -   " stackname
 
 read -p "please enter a region like us-east-1, us-east-2, us-west-2 -     " region
 
-echo "Creating nework stack with provided values !"
-aws cloudformation create-stack --stack-name "$stackname" --region "$region" --template-body file://application.json --parameters file://variables.json 2>/dev/null || { echo "A stack with this name already exist in the provided region. "; exit 1;}
+echo "Creating EC2 stack with provided values !"
+aws cloudformation create-stack --stack-name "$stackname" --region "$region" --capabilities CAPABILITY_NAMED_IAM --template-body file://application.json --parameters file://variables.json 2>/dev/null || { echo "A stack with this name already exist in the provided region. "; exit 1;}
